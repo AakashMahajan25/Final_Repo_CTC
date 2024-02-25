d6b453b207f714fd67e0bfb48d2e9e3ab2271bde
Kunal89204 <Kunalkhandelwal108@gmail.com>
2024-02-25 15:31:37 +0530

    social media icons added

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Img from "../assets/id8nxt.png";
+import { FaInstagram } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <div className="bg-slate-800 text-white lg:flex justify-between py-10">
-
       <div className="flex px-10">
         <div className="text-3xl font-semibold px-3">
           C2C <div>DEVCONVO</div>
@@ -15,14 +18,20 @@ const Footer = () => {
         </div>
       </div>
 
-<div className="px-14 lg:py-0 py-10">
-    <div>icons</div>
-    <div>
-        <div className="font-semibold">Get In Touch</div>
-        <div><a href="mailto:">info@societyofexcellance.in</a></div>
-    </div>
-</div>
-
+      <div className="px-14 lg:py-0 py-10">
+        <div className="flex gap-4 py-2">
+          <a href=""><FaInstagram className="text-xl" /></a>
+          <a href=""><FaFacebook className="text-xl" /></a>
+          <a href=""><FaSquareXTwitter className="text-xl" /></a>
+          <a href=""><FaLinkedin className="text-xl" /></a>
+        </div>
+        <div>
+          <div className="font-semibold">Get In Touch</div>
+          <div>
+            <a href="mailto:">info@societyofexcellance.in</a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
